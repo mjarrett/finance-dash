551ea40edeba337466e6ad4e0f1062a486d2be8d
Mike Jarrett <msjarrett@gmail.com>
2020-10-22 14:09:42 -0700

    adds filters to datatable

--- finance-dash.py
+++ finance-dash.py
@@ -2,8 +2,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import dash_auth
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
 import dash_table
 from dash.exceptions import PreventUpdate 
@@ -11,9 +13,10 @@ from dash.exceptions import PreventUpdate
 from credentials import *
 
 import pandas as pd
-import datetime
+import datetime as dt
 import json
 import numpy as np
+import re
 
 # colors = [
 #     '#1f77b4',  # muted blue
@@ -37,10 +40,10 @@ colors = {'Expenses':'#1f77b4',
 
 
 def make_dfs():
-    today = datetime.datetime.now()
-    limit = today-datetime.timedelta(30)
-    limit = datetime.datetime(limit.year,limit.month,1)
-    start = datetime.datetime(2019,1,1)
+    today = dt.datetime.now()
+    limit = today-dt.timedelta(30)
+    limit = dt.datetime(limit.year,limit.month,1)
+    start = dt.datetime(2019,1,1)
 
     start_str = start.strftime('%Y-%m-%d')
     limit_str = limit.strftime('%Y-%m-%d')
@@ -48,6 +51,8 @@ def make_dfs():
 
     #df_transactions = get_sheet(url,gid_expenses)
     #df_income = get_sheet(url,gid_income)
+    df_assets = get_sheet(url,gid_assets).applymap(tofloat)
+
     df_housing = get_sheet(url,gid_housing)
     df_savings = get_sheet(url,gid_savings)
     df_visa_transactions = get_sheet(url,gid_visa_transactions)
@@ -73,12 +78,14 @@ def make_dfs():
     sdf = sdf.loc[start:limit].reset_index()
     sdf.columns = ['Date','expenses','housing','savings','income']
     
+    
     sdf.to_csv('data/sdf.csv', index=False)
     df_transactions.to_csv('data/df_transactions.csv', index=True)
     df_expenses.to_csv('data/df_expenses.csv', index=True)
     df_income.to_csv('data/df_income.csv', index=True)
     df_housing.to_csv('data/df_housing.csv', index=True)
-    return sdf, df_transactions, df_expenses, df_income, df_housing
+    df_assets.to_csv('data/df_assets.csv',index=True)
+    return sdf, df_transactions, df_expenses, df_income, df_housing, df_assets
   
 def get_dfs():
     
@@ -93,8 +100,10 @@ def get_dfs():
         df_income.index = pd.to_datetime(df_income.index)
         df_housing = pd.read_csv('data/df_housing.csv', index_col='Date')
         df_housing.index = pd.to_datetime(df_housing.index)
+        df_assets = pd.read_csv('data/df_assets.csv',index_col='Date')
+        df_assets.index = pd.to_datetime(df_assets.index)
         
-        return sdf, df_transactions, df_expenses, df_income, df_housing
+        return sdf, df_transactions, df_expenses, df_income, df_housing, df_assets
         
     except FileNotFoundError:
         return make_dfs()
@@ -119,11 +128,18 @@ def make_table(df,value='Price'):
     return table
     
 print('Querying google doc')
+
+def tofloat(x):
+    if type(x) == float:
+        return x
+    x = re.sub("[^0-9\.]", "", x)
+    return float(x)
+
 def get_sheet(baseurl,gid,):
     url = f'{baseurl}/export?gid={gid}&format=csv'
 
     df = pd.read_csv(url,index_col='Date')
-    df.index = pd.to_datetime(df.index)
+    df.index = pd.to_datetime(df.index)  
 
     return df
 
@@ -142,13 +158,13 @@ auth = dash_auth.BasicAuth(
 #######
 
 
-def make_data_table(df):
+def make_datatable(df):
     df.index.name = 'Date'
     df = df.reset_index()
     df = df[['Date','Purchase','Price','Refund','Category']]
 
     table = dash_table.DataTable(
-        id='table',
+        id='transactions-table',
         columns=[{"name": i, "id": i} for i in df.columns],
         data=df.reset_index().to_dict('records'),
         editable=False,
@@ -166,10 +182,12 @@ def make_data_table(df):
         )
     return table
 
+ 
+    
 
 def make_fig1():
 
-    sdf, df_transactions, df_expenses, df_income, df_housing = get_dfs()
+    sdf, df_transactions, df_expenses, df_income, df_housing, df_assets = get_dfs()
 
     data = [
         go.Bar(
@@ -197,58 +215,182 @@ def make_fig1():
             marker_color=colors['Income']
             )
         ]
+    
+    data2 = [
 
-    layout = go.Layout(
+        go.Scatter(
+            x=df_assets.index, 
+            y=df_assets['Total Liquid'],
+            name='Cash + TFSA',
+            marker_color='#9467bd'
+            ),
+        go.Scatter(
+            x=df_assets.index, 
+            y=df_assets['Home value']-df_assets['Mortgage balance'],
+            name='Home Equity',
+            marker_color='#e377c2'
+            ),
+        go.Scatter(
+            x=df_assets.index, 
+            y=df_assets['Mike RRSP']+df_assets['Christa RRSP'],
+            name='RRSP',
+            marker_color='#17becf'
+            ),
+        
+        ]
+    
+    fig = make_subplots(rows=2, cols=1, 
+                    shared_xaxes=True, 
+                    vertical_spacing=0.09,
+                    subplot_titles=("Budget", "Assets"))
+    fig.add_trace(data[0], row=1,col=1)
+    fig.add_trace(data[1], row=1,col=1)
+    fig.add_trace(data[2], row=1,col=1)
+    fig.add_trace(data[3], row=1,col=1)
+    
+    fig.add_trace(data2[0], row=2,col=1)
+    fig.add_trace(data2[1], row=2,col=1)
+    fig.add_trace(data2[2], row=2,col=1)
+    
+    fig.update_layout(
         barmode = 'stack',
-        title = f'Budget'
+        dragmode='pan'
+        #title = f'Budget'
         )
-    fig1 = go.Figure(data=data,layout=layout)
-    return fig1
+    fig.update_xaxes(range=[sdf.index[-12],sdf.index[-1]])
+    fig.update_yaxes(fixedrange=True)
+
+    return fig
 
 
 
 
 def make_layout():
-    today = datetime.datetime.now()
-    limit = today-datetime.timedelta(30)
-    limit = datetime.datetime(limit.year,limit.month,1)
-    start = datetime.datetime(2019,1,1)
+    today = dt.datetime.now()
+    limit = today-dt.timedelta(30)
+    limit = dt.datetime(limit.year,limit.month,1)
+    start = dt.datetime(2019,1,1)
 
     start_str = start.strftime('%Y-%m-%d')
     limit_str = limit.strftime('%Y-%m-%d')
     year = today.strftime('%Y')
     
     
-    make_dfs()
-    sdf, df_transactions, df_expenses, df_income, df_housing = get_dfs()
-    print(df_transactions)
-    
+    #make_dfs()
+    sdf, df_transactions, df_expenses, df_income, df_housing, df_assets = get_dfs()
+    print(sdf.index)
     curr_savings = sdf.loc[limit_str, 'income'] - sdf.loc[limit_str, 'housing'] - sdf.loc[limit_str, 'expenses']
     ann_savings = sdf.loc[year,'income'].sum() - sdf.loc[year,'housing'].sum() - sdf.loc[year,'expenses'].sum()
     
-    return html.Div(children=[
-        html.H1(children='Houshold Finances'),
-
-        html.A("Here\'s the raw spreadsheet", href=raw_url),
-        html.Div(f"Savings this year: ${ann_savings:.2f}"),
-        html.Div(f"Savings this month: ${curr_savings:.2f}"),
-        
-        
-        html.Div(id='current-cat-data',style={'display':'none'}),
-        dcc.Graph(id='timeseries-graph',figure=make_fig1()),   
-
-        dcc.Graph(id='detail-graph',style={'display': 'none'}),
-        dcc.Graph(id='expense-timeseries',style={'display': 'none'}),
-        dcc.Interval(
-            id='interval-component',
-            interval=10*1000, # in milliseconds
-            n_intervals=0
+    controls = dbc.Card(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("Category"),
+                dcc.Dropdown(
+                    id="category-dropdown",
+                    options=[
+                       {"label": col, "value": col} for col in list(set(df_transactions['Category'].dropna())) + ['All']
+                    ],
+                    value='All',
+                ),
+            ]
         ),
-        html.Div([html.H3("Transactions"),make_data_table(df_transactions)])
+        dbc.FormGroup(
+            [
+                dbc.Label("Month"),
+                dcc.Dropdown(
+                    id="month-dropdown",
+                    options=[{"label": col.strftime('%b %Y'), "value": col.strftime('%Y-%m')} for col in sdf.index] + [{'label':'All','value':'All'}],
+                    value='All',
+                ),
+            ]
+        ),
+
+    ],
+    body=True,
+)
+    
+    
+    layout =  dbc.Container(children=[
+        html.H1(children='Houshold Finances'),
+        
+        dbc.Row([
+            dbc.Col([
+
+                html.A("Here\'s the raw spreadsheet", href=raw_url),
+                html.Div(f"Savings this year: ${ann_savings:.2f}"),
+                html.Div(f"Savings this month: ${curr_savings:.2f}"),
+                ]),
+            dbc.Col([
+                dbc.Button("Refresh data", size="lg", className="mr-1", id='refresh-button')
+            ])
+        ]),
+            
+        
+        dbc.Row([
+        
+            html.Div(id='current-cat-data',style={'display':'none'}),
+            dcc.Graph(id='timeseries-graph',figure=make_fig1()),  
+        ]),
+
+        dbc.Row([
+            dcc.Graph(id='detail-graph',style={'display': 'none'}),
+            dcc.Graph(id='expense-timeseries',style={'display': 'none'}),
+            dcc.Interval(
+                id='interval-component',
+                interval=10*1000, # in milliseconds
+                n_intervals=0
+            ),
+        ]),
+        
+        dbc.Row([
+            html.H3("Transactions"),
+            controls,
+            html.Div([make_datatable(df_transactions)])
+        ])
     ])
+    return layout
 
-app.layout = make_layout
+# wrap the layout in the content div so i can refresh everything with a callback
+app.layout = html.Div([
+    html.Div(id='content', children=make_layout())
+])
 
+
+
+
+# Refresh data
+@app.callback(Output('content', 'children'),
+              [Input('refresh-button','n_clicks')])
+def refresh_data(n_clicks):
+    print(n_clicks)
+    if n_clicks is not None:
+        make_dfs()
+        return make_layout()
+    
+    else:
+        raise PreventUpdate 
+
+# Update datatable
+@app.callback([Output('transactions-table','data')],
+              [Input('category-dropdown','value'),Input('month-dropdown','value')])
+def update_datatable(cat,month):
+    print(cat,month, type(month))
+    df = pd.read_csv('data/df_transactions.csv', index_col='Date')
+    df.index = pd.to_datetime(df.index)
+    
+    df.index.name = 'Date'
+    if month != 'All':
+        df = df[month]
+    df = df.reset_index()
+    df = df[['Date','Purchase','Price','Refund','Category']]
+    if cat != 'All':
+        df = df[df['Category']==cat]
+    
+    data = df.reset_index().to_dict('records')
+    
+    return data,
 
 @app.callback([Output('detail-graph','figure'), Output('detail-graph','style')],
               [Input('timeseries-graph','clickData')])
@@ -258,7 +400,7 @@ def make_detail_fig(clickData):
         raise PreventUpdate
         
         
-    sdf, df_transactions, df_expenses, df_income, df_housing = get_dfs()
+    sdf, df_transactions, df_expenses, df_income, df_housing, df_assets = get_dfs()
         
     date = clickData['points'][0]['x']
     cat  = clickData['points'][0]['curveNumber']
@@ -298,7 +440,7 @@ def make_detail_fig(clickData):
 def make_exp_fig(clickData,fig):
     print(clickData)
     
-    sdf, df_transactions, df_expenses, df_income, df_housing = get_dfs()
+    sdf, df_transactions, df_expenses, df_income, df_housing, df_assets = get_dfs()
     
     subcat = clickData['points'][0]['x']
     category = fig['layout']['title']['text']  
@@ -333,4 +475,4 @@ def make_exp_fig(clickData,fig):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False,host='0.0.0.0',port=8050)
+    app.run_server(debug=True,host='0.0.0.0',port=8050)
